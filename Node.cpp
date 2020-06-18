fa0f8104e40c197d64e9c4bd341e734980dcea01
wrawka <wrawka@gmail.com>
2020-06-18 18:33:48 +0300

    Node classes

--- Node.cpp
+++ Node.cpp
@@ -1,14 +1,66 @@
 #include "Node.h"
+#include "condition_parser.h"
 
 
-Node::Node(/* args */) {
 
-}
+bool Node::Evaluate(Date date, string condition) const {}
 
-Node::~Node() {
+bool EmptyNode::Evaluate(Date date, string condition) {return true;}
+
+// DateComparisonNode
+DateComparisonNode::DateComparisonNode(Comparison cmp, Date date) : op_(cmp), date_(date) {}
+
+bool DateComparisonNode::Evaluate(Date date, string condition) {
+    switch (op_)
+    {
+    case Comparison::Less :
+        return date < date_;
+        break;
     
+    case Comparison::LessOrEqual :
+        return !(date_ < date);
+        break;
+    
+    case Comparison::Greater :
+        return date_ < date;
+        break;
+    
+    case Comparison::GreaterOrEqual :
+        return !(date < date_);
+        break;
+    
+    case Comparison::Equal :
+        return date == date_;
+        break;
+    
+    case Comparison::NotEqual :
+        return !(date == date_);
+        break;
+    
+    default:
+        return true;
+        break;
+    }
 }
+// /DateComparisonNode
 
-bool Node::Evaluate(Date date, std::string condition) {
+// EventComparisonNode
+EventComparisonNode::EventComparisonNode(Comparison cmp, const string& event) : op_(cmp), event_(event) {}
 
+bool EventComparisonNode::Evaluate(Date date, string condition) {
+    switch (op_)
+    {
+    case Comparison::Equal:
+        return event_ == condition;
+        break;
+    
+    case Comparison::NotEqual:
+        return !(event_ == condition);
+        break;
+    
+    default:
+        return false;
+        break;
+    }
 }
+//  /EventComparisonNode
--- Node.h
+++ Node.h
@@ -7,12 +7,12 @@ private:
 public:
     Node(/* args */);
     ~Node();
-    virtual bool Evaluate(Date date, std::string condition) const = 0;
+    virtual bool Evaluate(Date date, string condition) const = 0;
 };
 
 
 class EmptyNode : public Node {
-
+    virtual bool Evaluate(Date date, string condition);
 };
 
 class LogicalOperationNode : public Node {
@@ -21,14 +21,23 @@ class LogicalOperationNode : public Node {
 
 class DateComparisonNode : public Node {
 private:
-
+    const Comparison op_; // операция сравнения
+    const Date date_; // дата-референс
+    
 public:
     DateComparisonNode();
     DateComparisonNode(Comparison cmp, Date date);
     ~DateComparisonNode();
+    virtual bool Evaluate(Date date, string condition);
 };
 
 class EventComparisonNode : public Node {
+private:
+    const Comparison op_; // операция сравнения
+    const string event_; // событие-референс
+    virtual bool Evaluate(Date date, string condition);
+
+public:
     EventComparisonNode();
     EventComparisonNode(Comparison cmp, const string& event);
     ~EventComparisonNode();
